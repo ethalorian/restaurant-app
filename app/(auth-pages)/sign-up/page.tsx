26c35b95cee7279ae858a074d00f2f61e1c8c3db
Craig Antocci <eth@craigs-mac-mini.mynetworksettings.com>
2024-09-24 17:15:26 -0400

    fix type error for google button

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -44,7 +44,9 @@ export default function Signup({ searchParams }: { searchParams: Message }) {
           </SubmitButton>
           <FormMessage message={searchParams} />
           <div className="mt-2">
-            <GoogleSignInButton className="w-full bg-white text-gray-700 font-medium py-2 px-4 border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary" />
+            <div className="w-full bg-white text-gray-700 font-medium py-2 px-4 border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary">
+              <GoogleSignInButton />
+            </div>
           </div>
         </div>
       </form>
