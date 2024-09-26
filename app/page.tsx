3c0447d3c7b497f26a157087fdd7dbb9060b8fec
Craig Antocci <eth@craigs-mac-mini.mynetworksettings.com>
2024-09-26 17:09:10 -0400

    Improved Landing Layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,17 @@ export default async function Index() {
   return (
     <>
       <Hero />
-      <main className="grid grid-cols-1 md:grid-cols-6 gap-4 px-4">
-        {/* Restaurants component takes 2/3 width on non-md screens and starts in column 1 */}
-        <div className="col-span-1 md:col-span-3">
+      <main className="grid grid-cols-1 md:grid-cols-6 gap-4 md:gap-5 px-[5px] md:px-4">
+        {/* Empty column 1 */}
+        <div className="hidden md:block md:col-span-1"></div>
+        
+        {/* Restaurants component takes up columns 2-5 on md screens */}
+        <div className="col-span-1 md:col-start-2 md:col-span-4 px-10 md:px-0">
           <Restaurants />
         </div>
-        {/* You can add other content in the remaining 1/3 space if needed */}
-        <div className="col-span-1">
-          {/* Additional content can go here */}
-        </div>
+        
+        {/* Empty column 6 */}
+        <div className="hidden md:block md:col-span-1"></div>
       </main>
     </>
   );
